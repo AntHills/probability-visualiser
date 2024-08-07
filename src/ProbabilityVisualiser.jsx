a78e098eda148b009b186b3562c94a26da69a698
James W <jamesweber1994@gmail.com>
2024-08-08 09:43:29 +1000

    added calculation functionality

--- src/ProbabilityVisualiser.jsx
+++ src/ProbabilityVisualiser.jsx
@@ -2,14 +2,24 @@ import React, { useState, useRef, useEffect } from "react";
 
 function ProbabilityVisualiser() {
   const [percentage, setPercentage] = useState(50);
-  const [probability, setProbability] = useState("100");
+  const [probability, setProbability] = useState("128");
   const [attempts, setAttempts] = useState(0);
   const probabilityInputRef = useRef(null);
 
   useEffect(() => {
     probabilityInputRef.current.style.width =
       probabilityInputRef.current.value.length + "ch";
+
+    setPercentage(calcPercentage());
   }, [probability]);
+
+  // useEffect(() => {
+  //   setPercentage(calcPercentage());
+  // }, [attempts]);
+
+  useEffect(() => {
+    setAttempts(calcAttempts());
+  }, [percentage]);
 
   function handleSliderChange(event) {
     setPercentage(event.target.value);
@@ -24,21 +34,37 @@ function ProbabilityVisualiser() {
     setAttempts(event.target.value);
   }
 
+  function calcPercentage() {
+    const newPercentage =
+      (1 - Math.pow((probability - 1) / probability, attempts)) * 100;
+    return newPercentage.toFixed(2);
+  }
+
+  function calcAttempts() {
+    const percentageDecimal = percentage / 100;
+    const newAttempts =
+      Math.log(1 - percentageDecimal) /
+      Math.log((probability - 1) / probability);
+    return newAttempts.toFixed(0);
+  }
+
   return (
     <>
-      <div className="visualiser-container">
+      <div className="app-container">
         <h1>How unlucky am I really?</h1>
-        <p>
-          When estimating the probability of an event occurring it is common for
-          this to be misinterpreted. For example, if there is an event that is
-          1/100 change of occurring, if you make an attempt 100 times the
-          probabiliy that it will occur is ~64% chance of having a successful
-          attempt (not 100%)
-        </p>
-        <div className="display-container">
-          <span className="percentage-display">{percentage}%</span>
+        <div className="explanation-text">
+          <span>
+            If you attempt to recieve a prize with a 1/100 chance, you might
+            think that attempting 100 times would give you a close to 100%
+            chance of recieving the prize. However the actual percentage chance
+            of recieving a prize in that scenario would be ~63%. Use the
+            calculator below to visual how attempts effect your chance of
+            winning
+          </span>
+        </div>
+        <div>
           <span className="probability-display">
-            1/
+            If an event has a 1/
             <input
               className="probability-input"
               type="number"
@@ -46,26 +72,36 @@ function ProbabilityVisualiser() {
               placeholder="100"
               onChange={handleProbabilityChange}
               ref={probabilityInputRef}
-            />
+            />{" "}
+            of occurring
           </span>
-          <span className="attempts-display">{attempts}</span>
         </div>
-        <div className="controls">
-          <input
-            type="number"
-            value={attempts}
-            onChange={handleAttemptsChange}
-          />
-          <input
-            type="range"
-            min="0"
-            max="100"
-            step="0.5"
-            value={percentage}
-            class="slider"
-            id="myRange"
-            onChange={handleSliderChange}
-          />
+        <div className="visualiser-container">
+          <div className="display-container">
+            <div className="percentage-container">
+              <span className="percentage-display">{percentage}%</span>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                step="0.1"
+                value={percentage}
+                className="slider"
+                id="myRange"
+                onChange={handleSliderChange}
+              />
+            </div>
+          </div>
+          <div className="display-attempts-container">
+            <div>
+              <span className="attempts-display">{attempts}</span>
+            </div>
+            <input
+              type="number"
+              value={attempts}
+              onChange={handleAttemptsChange}
+            />
+          </div>
         </div>
       </div>
     </>
