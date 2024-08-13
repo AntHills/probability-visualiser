ccf5d32f9f8c098d59c8eaf1737443075603d6c6
James W <jamesweber1994@gmail.com>
2024-08-13 22:59:05 +1000

    Added GA support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import ProbabilityVisualiser from "./ProbabilityVisualiser.jsx";
+import ReactGA from "react-ga4";
+import React, { useEffect } from "react";
+
+ReactGA.initialize("G-375PSGNH4R");
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <ProbabilityVisualiser />
